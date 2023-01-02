1038c1989511db165da5c5d3c041dedce4f06ea0
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2023-01-02 16:25:15 -0300

    commit dos exemplos

---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TipoVariaveis {
+
+	public static void main(String[] args) {
+		
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero = 5;
+		
+		numero = 10;
+		
+		System.out.println("Valor: " + numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		System.out.println("Valor de PI:" + VALOR_DE_PI);
+	}
+
+}
